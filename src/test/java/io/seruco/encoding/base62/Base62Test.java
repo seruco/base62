2806f3a189df1369e5adbf9854caf8b4dd04201a
Sebastian Ruhleder <sebastian.ruhleder@nterra.com>
2017-12-10 23:25:58 +0100

    test: create Environment class and refactor Base62Test accordingly

--- src/test/java/io/seruco/encoding/base62/Base62Test.java
+++ src/test/java/io/seruco/encoding/base62/Base62Test.java
@@ -15,8 +15,6 @@ public class Base62Test {
 
     private final Base62 standardEncoder = Base62.createStandardEncoder();
 
-    private final Base62 invertedEncoder = Base62.createInvertedEncoder();
-
     private final Base62[] encoders = {
             Base62.createStandardEncoder(),
             Base62.createInvertedEncoder()
@@ -25,7 +23,7 @@ public class Base62Test {
     @Test
     @DisplayName("should preserve identity of simple byte arrays")
     public void preservesIdentity() {
-        for (byte[] message : rawInputs) {
+        for (byte[] message : Environment.getRawInputs()) {
             for (Base62 encoder : encoders) {
                 final byte[] encoded = encoder.encode(message);
                 final byte[] decoded = encoder.decode(encoded);
@@ -38,7 +36,7 @@ public class Base62Test {
     @Test
     @DisplayName("should produce encodings that only contain alphanumeric characters")
     public void alphaNumericOutput() {
-        for (byte[] message : rawInputs) {
+        for (byte[] message : Environment.getRawInputs()) {
             for (Base62 encoder : encoders) {
                 final byte[] encoded = encoder.encode(message);
                 final String encodedStr = new String(encoded);
@@ -65,67 +63,16 @@ public class Base62Test {
     @Test
     @DisplayName("should behave correctly on naive test set")
     public void naiveTestSet() {
-        for (Map.Entry<String, String> testSetEntry : createNaiveTestSet().entrySet()) {
+        for (Map.Entry<String, String> testSetEntry : Environment.getNaiveTestSet().entrySet()) {
             assertEquals(encode(testSetEntry.getKey()), testSetEntry.getValue());
         }
-    }
-
-    private final byte[][] rawInputs = {
-            createIncreasingByteArray(),
-            createZeroesByteArray(512),
-            createPseudoRandomByteArray(0xAB, 40),
-            createPseudoRandomByteArray(0x1C, 40),
-            createPseudoRandomByteArray(0xF2, 40)
-    };
-
-    private Map<String, String> createNaiveTestSet() {
-        final Map<String, String> testSet = new HashMap<String, String>();
-
-        testSet.put("", "");
-        testSet.put("Hello", "5TP3P3v");
-        testSet.put("0123456789", "18XU2xYejWO9d3");
-        testSet.put("The quick brown fox jumps over the lazy dog", "83UM8dOjD4xrzASgmqLOXTgTagvV1jPegUJ39mcYnwHwTlzpdfKXvpp4RL");
-        testSet.put("Sphinx of black quartz, judge my vow", "1Ul5yQGNM8YFBp3sz19dYj1kTp95OW7jI8pTcTP5JhYjIaFmx");
-
-        return testSet;
-    }
-
-    private boolean isAlphaNumeric(final String str) {
-        return str.matches("^[a-zA-Z0-9]+$");
     }
 
     private String encode(final String input) {
         return new String(standardEncoder.encode(input.getBytes()));
     }
 
-    private byte[] createIncreasingByteArray() {
-        final byte[] arr = new byte[256];
-        for (int i = 0; i < 256; i++) {
-            arr[i] = (byte) (i & 0xFF);
-        }
-        return arr;
-    }
-
-    private byte[] createZeroesByteArray(int size) {
-        return new byte[size];
-    }
-
-    private byte[] createPseudoRandomByteArray(int seed, int size) {
-        final byte[] arr = new byte[size];
-        int state = seed;
-        for (int i = 0; i < size; i += 4) {
-            state = xorshift(state);
-            for (int j = 0; j < 4 && i + j < size; j++) {
-                arr[i + j] = (byte) ((state >> j) & 0xFF);
-            }
-        }
-        return arr;
-    }
-
-    private int xorshift(int x) {
-        x ^= (x << 13);
-        x ^= (x >> 17);
-        x ^= (x << 5);
-        return x;
+    private boolean isAlphaNumeric(final String str) {
+        return str.matches("^[a-zA-Z0-9]+$");
     }
 }
